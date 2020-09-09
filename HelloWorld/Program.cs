213e9c50f26bfa8d598f5f144d130a8dc1b359b4
Nelsoniwe <texno090@gmail.com>
2020-09-09 13:17:30 +0300

    Second commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); //here will be the project
+            Console.WriteLine("Hello Space!"); //here will be the project
             Console.Read();
         }
     }
